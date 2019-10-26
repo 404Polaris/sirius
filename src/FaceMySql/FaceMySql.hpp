c4cc58a3044c406471635ebf6c8ee99d5e011ec8
404Polaris <574706224@qq.com>
2019-10-26 20:21:30 +0800

    init

--- src/FaceMySql/FaceMySql.hpp
+++ src/FaceMySql/FaceMySql.hpp
@@ -8,6 +8,23 @@ extern "C" {
 #include <mysql.h>
 }
 
-namespace FaceMySql {
+#include <string>
 
+namespace FaceMySql {
+	template<typename _Object_type, typename _Field_type>
+	struct Field {
+		std::string field_name_;
+		_Field_type _Object_type::* field_;
+	};
+
+	template<typename _DbAdapter_type>
+	auto MakeFace(typename _DbAdapter_type::_Config_type config) {
+
+	}
+
+	class MySqlAdapter {
+		struct Config {};
+	public:
+		using _Config_type = Config;
+	};
 }
